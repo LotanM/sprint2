48f8a36fda6c11ef15317b08ae6398041caab9a7
Lotan <lotan.mash@gmail.com>
2021-02-10 16:08:20 +0200

    canvas on the way

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 'use strict'
+var gElCanvas, gCtx;
 
-var gKeywords = {
-    'happy': 12,
-    'funny': 1,
-    'animal': 1
+
+
+function getCanvas() {
+    gElCanvas = document.getElementById('my-canvas');
+    gCtx = gElCanvas.getContext('2d');
+    drawImg()
 }
-var gImgs = [{
-    id: 1,
-    url: 'img\meme-imgs-square\1.jpg',
-    keywords: ['happy']
-}];
 
-var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I never eat Falafel',
-            size: 20,
-            align: 'left',
-            color: 'red'
-        }
-    ]
+function drawImg() {
+    const img = new Image()
+    img.src = gImgs[2].url;
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+    }
+}
+
+
+function drawTxt() {
+    var text = gMeme.lines[0].txt
+    var font = gMeme.lines[0].font
+    var fontSize = gMeme.lines[0].size
+    var align = gMeme.lines[0].align
+    var color = gMeme.lines[0].color
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'white'
+    gCtx.fillStyle = color
+    gCtx.font = fontSize + 'px ' + font
+    gCtx.textAlign = align
+    var x = 100
+    var y = 100
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
 }
---
+++ js/meme-service.js
@@ -0,0 +1,43 @@
+'use strict'
+
+
+var gKeywords = {
+    'happy': 0,
+    'funny': 1,
+    'animal': 1,
+    'men': 1
+}
+
+var gImgs = [{
+    id: 1,
+    url: 'img/meme-imgs-square/1.jpg',
+    keywords: ['men', 'funny']
+}, {
+    id: 2,
+    url: 'img/meme-imgs-square/2.jpg',
+    keywords: ['animal']
+}, {
+    id: 3,
+    url: 'img/meme-imgs-square/3.jpg',
+    keywords: ['animal', 'funny']
+}]
+
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I never eat Falafel',
+            size: 40,
+            align: 'left',
+            color: 'black',
+            font: 'Impact'
+        }
+    ]
+}
+
+
+function addTxt() {
+
+}
