14fdab6f56d742e948fd0cafa1cf4986d9cd8b9d
Lotan <lotan.mash@gmail.com>
2021-02-10 13:50:31 +0200

    added js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 'use strict'
+
+var gKeywords = {
+    'happy': 12,
+    'funny': 1,
+    'animal': 1
+}
+var gImgs = [{
+    id: 1,
+    url: 'img\meme-imgs-square\1.jpg',
+    keywords: ['happy']
+}];
+
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I never eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
